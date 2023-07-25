6985d45f8056eb89c8392c5c4b85e4a0953a80ba
Victor Matheus <victormath23@outlook.com>
2023-07-25 17:59:39 -0300

    alterando destino das imagens

--- script.js
+++ script.js
@@ -8,20 +8,20 @@ function carregar(){
 
     msg.innerHTML = `Agora são ${hora} horas`
     if(hora >=0 && hora < 12){
-        img.src = '../Aula12ex/imagens/manha.jpg'
+        img.src = 'imagens/manha.jpg'
         document.body.style.background = 'rgb(218, 215, 194)'
         msg.style.color = 'rgb(218, 215, 194)'
         
     }
     else if(hora >=12 && hora <=18){
-        img.src = '../Aula12ex/imagens/tarde.jpg'
+        img.src = 'imagens/tarde.jpg'
         document.body.style.background = 'rgb(198, 141, 108)'
         msg.style.color = 'rgb(198, 141, 108)'
         
         
     }
     else{
-        img.src = '../Aula12ex/imagens/noite.jpg'
+        img.src = 'imagens/noite.jpg'
         document.body.style.background = 'rgb(25, 50, 83)'
         msg.style.color = 'rgb(25, 50, 83)'
     }
